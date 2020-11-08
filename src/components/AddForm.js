1ad70e23d0a1d685a0b13bfe837f55ce0134f351
Kohta Takanami <ripplekouha@gmail.com>
2020-11-08 13:14:05 +0900

    [add]メモ追加コンポーネントを作成

--- src/components/AddForm.js
+++ src/components/AddForm.js
@@ -1,6 +1,42 @@
 import { Component } from 'react';
+import {addNote} from "../stores/NoteStore";
 
 class AddForm extends Component{
+    input = {
+        fontSize: "16pt",
+        color: "#006",
+        padding: "1px",
+        margin: "5px 0px"
+    }
+    btn = {
+        fontSize: "16pt",
+        color: "#006",
+        padding: "2px 10px",
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: ''
+        }
+        this.doChange = this.doChange.bind(this);
+        this.doAction = this.doAction.bind(this);
+    }
+
+    doChange(e) {
+        this.setState({
+            message: e.target.value
+        })
+    }
+
+    doAction(e) {
+        e.preventDefault();
+        let action = addNote(this.state.message);
+        this.props.dispatch(action);
+        this.setState({
+            message: ''
+        })
+    }
 
     render() {
         return;
