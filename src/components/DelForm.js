3557ec7f2e2b20fefc593ad7cd20579e45b80514
Kohta Takanami <ripplekouha@gmail.com>
2020-11-08 14:07:08 +0900

    [add]デリートフォームコンポーネントを作成

--- src/components/DelForm.js
+++ src/components/DelForm.js
@@ -1,12 +1,67 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
+import { deleteNote } from "../stores/Store";
+import { connect } from "react-redux";
 
 class DelForm extends Component {
+    input = {
+        fontSize: "12pt",
+        color: "#006",
+        padding: "1px",
+        margin: "5px 0px"
+    }
+    btn = {
+        fontSize: "10pt",
+        color: "#006",
+        padding: "2px 10px",
+    }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: 0
+        }
+        this.doAction = this.doAction.bind(this);
+        this.doChange = this.doChange.bind(this);
+    }
+
+    doChange(e) {
+        this.setState({
+            number: e.target.value
+        });
+    }
+
+    doAction(e) {
+        e.preventDefault();
+        let action = deleteNote(this.state.number);
+        this.props.dispatch(action);
+        this.setState({
+            number: 0
+        });
+    }
+
 
     render() {
+        let n = 0;
+        let items = this.props.data.map( value => (
+            <option key={n} value={n++}>
+                {value.message.substring(0,10)}
+            </option>
+        ))
         return (
-            <div></div>
+            <div>
+                <form onSubmit={this.doAction}>
+                    <select
+                        onChange={this.doChange}
+                        defaultValue="-1"
+                        style={this.input}
+                    >
+                        {items}
+                    </select>
+                    <input type="submit" style={this.btn} value="Del" />
+                </form>
+            </div>
         );
     }
 }
 
-export default DelForm;
+export default connect( state => state)(DelForm);
