5e5a2de886312831747dea3f53557380c622af76
Kohta Takanami <ripplekouha@gmail.com>
2020-11-08 12:56:51 +0900

    [add]Itemコンポーネントを作成

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,42 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
 class Item extends Component {
+    th = {
+        fontSize: "14pt",
+        backgroundColor: "blue",
+        color: "white",
+        padding: "5px 10px",
+        width: "50px"
+    }
+
+    td = {
+        fontSize: "14pt",
+        backgroundColor: "white",
+        color: "darkblue",
+        padding: "5px 10px",
+        border: "1px slid lightblue",
+        minWidth: "300px"
+    }
+
+    date = {
+        fontSize: "14pt",
+        backgroundColor: "white",
+        color: "darkblue",
+        padding: "5px 10px",
+        border: "1px slid lightblue",
+        minWidth: "80px"
+    }
 
     render() {
-        return;
+        let at = this.props.value.created;
+        let time = at.getHours() + ':' + at.getMinutes() + ':' + at.getSeconds();
+        return (
+            <tr>
+                <th style={this.th}>No, {this.props.index}</th>
+                <td style={this.td}>{this.props.value.message}</td>
+                <td style={this.date}>{time}</td>
+            </tr>
+        );
     }
 }
 
